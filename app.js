0d314ad88ba3ac6b5ac0047d5ae860265a4ea4f8
Ankit Bisen <76652297+ankitbisen12@users.noreply.github.com>
2024-05-12 10:18:30 +0530

    Adding vercel.json

--- app.js
+++ app.js
@@ -20,40 +20,49 @@ const User = require('./models/userModel');
 const { isAuth, sanitizer, cookieExtractor } = require('./utils/common');
 const mailRouter = require('./routes/mailRoutes');
 const path = require('path');
+const Order = require('./models/orderModel');
 
 //webhook
 //TODO: we will capture actual order after deploying out server live on public url.
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 const endpointSecret = process.env.ENDPOINT_SECRET;
 
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
+app.post(
+  '/webhook',
+  express.raw({ type: 'application/json' }),
+  async (request, response) => {
+    const sig = request.headers['stripe-signature'];
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntentSucceeded = event.data.object;
+        console.log({ paymentIntentSucceeded });
+        const order = await Order.findById(
+          paymentIntentSucceeded.metadata.orderId
+        );
+        order.paymentStatus = 'received';
+        await order.save();
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntentSucceeded = event.data.object;
-      console.log({paymentIntentSucceeded});
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
-
+);
 
 //Jwt options
 const opts = {};
@@ -61,7 +70,7 @@ opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = process.env.JWT_SECRET_KEY; //TODO: this should not be in code.
 
 //Middlewares
-app.use(express.static(path.resolve(__dirname,'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(cookieParser());
 app.use(
   session({
@@ -89,6 +98,10 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/cart', isAuth(), cartRouter);
 app.use('/api/v1/orders', isAuth(), orderRouter);
 app.use('/api/v1/mail', mailRouter);
+//this line we add to make react route to work in case of other routes doesn't match
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('build', 'index.html'));
+});
 
 //Passport Strategy i.e local Strategy
 passport.use(
@@ -167,17 +180,20 @@ passport.deserializeUser(function (user, cb) {
 
 //Payments
 // This is your test secret API key.
-const stripe = require("stripe")('sk_test_51PFCuwSAmgkvZkxcVrb0HNcDMF86FncQiXbDlcmLjAyErE6o42Dgo9cV8sGbSno8M93GNQMhhkPjn5UN2e5lWbYd00viPYZIWQ');
+const stripe = require('stripe')(
+  'sk_test_51PFCuwSAmgkvZkxcVrb0HNcDMF86FncQiXbDlcmLjAyErE6o42Dgo9cV8sGbSno8M93GNQMhhkPjn5UN2e5lWbYd00viPYZIWQ'
+);
 
-app.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount ,selectedAddress} = req.body;
-  console.log(totalAmount,selectedAddress);
+app.post('/create-payment-intent', async (req, res) => {
+  const { order, orderId } = req.body;
+  const selectedAddress = order.selectedAddress;
+  // console.log(totalAmount,selectedAddress);
   // console.log(req.body);
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: totalAmount*100, //for decimal compensation
-    currency: "inr",
+    amount: order.totalAmount * 100, //for decimal compensation
+    currency: 'inr',
     shipping: {
       name: `${selectedAddress.name}`,
       address: {
@@ -188,12 +204,15 @@ app.post("/create-payment-intent", async (req, res) => {
         country: 'IN',
       },
     },
-    description:"Payment completed",
+    description: 'Payment completed',
     payment_method_types: ['card'],
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     // automatic_payment_methods: {
     //   enabled: true,
     // },
+    metadata: {
+      orderId,
+    },
   });
 
   res.send({
@@ -201,11 +220,9 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-
 app.all('*', (req, res, next) => {
   console.log(`Can't find ${req.originalUrl} on this server`, 404);
 });
-
 
 // app.get('/',(req,res)=>{
 //     res.json({status:'success'});
